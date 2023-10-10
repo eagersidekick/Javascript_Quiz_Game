6d3dad573ec711d3a2f005da3d97f3a6cf513ed6
Heather Crawford <heather.crawford.ela@gmail.com>
2023-10-10 14:26:04 -0500

    added function to process answers as they are selected and move to the next question.

--- js/script.js
+++ js/script.js
@@ -1,43 +1,66 @@
+//variables
+var choicesElement = document.getElementById("choices");
+var questionIndex = 0;
+var quizGrade = 0;
+
 //Question bank
 var question1 = {
-    number: "1",
     title: "What is your name?",
     choices: {
         a: "Heather",
         b: "Halle",
         c: "Brian"
     },
-    answer: "A"
+    answer: "a"
 };
 
 var question2 = {
-    number: "2",
     title: "What is your favorite color?",
     choices: {
         a: "Teal",
         b: "Pink",
         c: "Purple",
     },
-    answer: "C"
+    answer: "c"
 };
 
 //question order
-//var questionOrder = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10];
+var questionOrder = [question1, question2];
+//, question3, question4, question5, question6, question7, question8, question9, question10
 
 //function to display questions one at a time
 function displayQuiz(question) {
     document.getElementById("question").innerHTML = question.title;
-
-    var choicesElement = document.getElementById("choices");
     choicesElement.innerHTML = "";
-    choicesElement.setAttribute("name", question.number);
 
     Object.entries(question.choices).forEach(([letter, text]) => {
         const newButton = document.createElement('button');
         newButton.textContent = text;
         newButton.name = letter;
         newButton.className = "buttons";
-        choicesElement.appendChild(newButton);
-    });       
+        choicesElement.appendChild(newButton);     
+        newButton.addEventListener("click", processAnswer);
+    });
+}
+//function to process what happens after an answer button is pressed.
+function processAnswer(event) {
+    var answer = questionOrder[questionIndex].answer;
+    if (event.target.getAttribute("name") === answer) {
+        quizGrade += 10;
+        document.getElementById("score").innerHTML = "Your score: " + quizGrade + " out of 100";
+        document.getElementById("message").innerHTML = "Congrats! That's correct!";
+    }
+    else document.getElementById("message").innerHTML = "Sorry, you got it wrong!";
+        document.getElementById("score").innerHTML = "Your score: " + quizGrade + " out of 100";
+    questionIndex++;
+    if (questionOrder.length === questionIndex) displayLeaderboard();
+    else displayQuiz(questionOrder[questionIndex]);
 }
 
+//function for displaying the leaderboard
+function displayLeaderboard() {
+
+}
+
+//starts the quiz
+displayQuiz(question1);
